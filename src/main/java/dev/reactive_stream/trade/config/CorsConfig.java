042909c2c9042ba58e7338491703f3f1128d1592
urimL <toulim0525@gmail.com>
2026-04-14 09:53:19 +0900

    init

--- src/main/java/dev/reactive_stream/trade/config/CorsConfig.java
+++ src/main/java/dev/reactive_stream/trade/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "OPTIONS");
     }
 }
